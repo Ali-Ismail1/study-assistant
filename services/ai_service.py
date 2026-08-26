078b963ef3d581ca75df3b2329cb5ab0e22cd625
Ali Ismail <ali.mohamed.ket@gmail.com>
2026-08-26 14:57:06 +0300

    added error handling in ai_services.py and fixed issue in pdf_service.py

--- services/ai_service.py
+++ services/ai_service.py
@@ -12,26 +12,33 @@ client = genai.Client(api_key = api_key)
 
 def get_ai_summary(pdf_text : str ):
     final_prompt = f'Provide a detailed summary for the following text ( summary must be shorter than original text length): \n {pdf_text}'
-    ai_result = client.models.generate_content(
-        model = "gemini-3.6-flash", 
-        contents = final_prompt
-        )
-    return {"summary": ai_result.text}
-
+    try:
+        ai_result = client.models.generate_content(
+            model = "gemini-3.6-flash", 
+            contents = final_prompt
+            )
+        return {"summary": ai_result.text}
+    except Exception as e:
+        raise ValueError(f"Gemini API error: {str(e)}")
 def get_ai_quiz(pdf_text : str ):
     final_prompt = f'Generate a 10 question quiz covering the key ropics in the following text , the questions may be mcq , article , true or false etc ... : \n {pdf_text}'
-    ai_result = client.models.generate_content(
-        model = "gemini-3.6-flash", 
-        contents = final_prompt
-        )
-    return {"quiz": ai_result.text}
-
+    try:
+        ai_result = client.models.generate_content(
+            model = "gemini-3.6-flash", 
+            contents = final_prompt
+            )
+        return {"quiz": ai_result.text}
+    except Exception as e:
+        raise ValueError(f"Gemini API error: {str(e)}")
 def get_ai_concepts (pdf_text : str ):
     final_prompt = f'state the key concepts and Provide an explanation for each from  the following text  with refrence to samples from the text during explanation of key concepts : \n {pdf_text}'
-    ai_result = client.models.generate_content(
-        model = "gemini-3.6-flash", 
-        contents = final_prompt
-        )
-    return {"key concepts ": ai_result.text}
+    try:
+        ai_result = client.models.generate_content(
+            model = "gemini-3.6-flash", 
+            contents = final_prompt
+            )
+        return {"key_concepts ": ai_result.text}
+    except Exception as e:
+        raise ValueError(f"Gemini API error: {str(e)}")
 
 
--- services/pdf_service.py
+++ services/pdf_service.py
@@ -3,5 +3,5 @@ import io
 
 def get_text(pdf_bytes) :
     reader= pypdf.PdfReader(io.BytesIO(pdf_bytes))
-    pdf_text = "\n".join([r.extract_text() for r in reader.pages() if r.extract_text()])
+    pdf_text = "\n".join([r.extract_text() for r in reader.pages if r.extract_text()])
     return pdf_text
